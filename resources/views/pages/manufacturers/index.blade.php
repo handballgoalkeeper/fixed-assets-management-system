bff3a41bd9633265fd19fd7d2526335b6ca986d8
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-08 22:04:57 +0100

    * Added pagination to manufacturers main page

--- resources/views/pages/manufacturers/index.blade.php
+++ resources/views/pages/manufacturers/index.blade.php
@@ -5,23 +5,19 @@
 @endsection
 
 @section('content')
-    @if(!is_null(session('success')))
-        <div class="alert alert-success" role="alert">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if(!is_null(session('error')))
-        <div class="alert alert-danger" role="alert">
-            {{ session('error') }}
-        </div>
-    @endif
-    <div class="table-responsive">
+    @include('partials.successAlert')
+    @include('partials.errorAlert')
+    <div class="d-flex justify-content-center mt-4">
+        {{ $manufacturers->onEachSide(2)->links('pagination::bootstrap-5') }}
+    </div>
+    <div class="container-flow table-responsive">
         <table class="table table-striped table-responsive-sm">
             <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Name</th>
                     <th scope="col">Description</th>
+                    <th scope="col">Status</th>
                     <th scope="col">Actions</th>
                 </tr>
             </thead>
@@ -31,14 +27,25 @@
                     <th scope="row">{{ $loop->iteration }}</th>
                     <td>{{ $manufacturer->name }}</td>
                     <td>{{ $manufacturer->description }}</td>
+                    @if( $manufacturer->is_active === 1 )
+                    <td class="text-success">Active</td>
+                    @else
+                    <td class="text-danger">Inactive</td>
+                    @endif
                     <td>
                         <a href="{{ route(name: 'manufacturers.permalink', parameters: [ 'manufacturer' => $manufacturer->id] ) }}" class="btn btn-outline-primary">
                             View
+                        </a>
+                        <a href="{{ route(name: 'manufacturers.history', parameters: [ 'manufacturer' => $manufacturer->id] ) }}" class="btn btn-outline-secondary">
+                            History
                         </a>
                     </td>
                 </tr>
             @endforeach
             </tbody>
         </table>
+    </div>
+    <div class="d-flex justify-content-center mt-4">
+        {{ $manufacturers->onEachSide(2)->links('pagination::bootstrap-5') }}
     </div>
 @endsection
