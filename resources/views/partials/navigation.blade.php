ff23344eeacf0c388a54eb11bac64584426f7115
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-08 19:14:00 +0100

    * Updated navigation brand

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,7 +1,7 @@
 <div class="container-flow">
     <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('home') }}">{{ \App\Configs\AppConfig::SHORT_APP_NAME }}</a>
+            <a class="navbar-brand fw-bold text-success fs-4 mx-4" href="{{ route('home') }}">{{ \App\Configs\AppConfig::SHORT_APP_NAME }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
