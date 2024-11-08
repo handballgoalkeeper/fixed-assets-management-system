f0ede10cb4e065fb1733e0be185f89ed4481f30d
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-08 15:36:57 +0100

    * Added manufacturer observer, implemented created hook

---
+++ app/Observers/ManufacturerModelObserver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\ManufacturerModel;
+use App\Services\ManufacturerHistoryService;
+
+class ManufacturerModelObserver
+{
+    /**
+     * Handle the ManufacturerModel "created" event.
+     */
+    public function created(ManufacturerModel $manufacturerModel): void
+    {
+        app(ManufacturerHistoryService::class)->handleManufacturerCreated($manufacturerModel);
+    }
+
+    /**
+     * Handle the ManufacturerModel "updated" event.
+     */
+    public function updated(ManufacturerModel $manufacturerModel): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ManufacturerModel "deleted" event.
+     */
+    public function deleted(ManufacturerModel $manufacturerModel): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ManufacturerModel "restored" event.
+     */
+    public function restored(ManufacturerModel $manufacturerModel): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ManufacturerModel "force deleted" event.
+     */
+    public function forceDeleted(ManufacturerModel $manufacturerModel): void
+    {
+        //
+    }
+}
