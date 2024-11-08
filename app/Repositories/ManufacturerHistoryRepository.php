45556096af55725eed1d606e2fd41dfa38affe80
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-08 15:38:26 +0100

    * Added manufacturer repository and implemented save method

---
+++ app/Repositories/ManufacturerHistoryRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Exceptions\GeneralException;
+use App\Models\ManufacturerHistoryModel;
+use App\Repositories\CrudRepository;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class ManufacturerHistoryRepository implements CrudRepository
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function findAll(): Collection
+    {
+        // TODO: Implement findAll() method.
+    }
+
+    /**
+     * @inheritDoc
+     * @throws GeneralException
+     */
+    public function save(ManufacturerHistoryModel|Model $model): void
+    {
+//        try {
+        $model->save();
+//        }
+//        catch (Exception $e) {
+//            throw new GeneralException();
+//        }
+    }
+}
