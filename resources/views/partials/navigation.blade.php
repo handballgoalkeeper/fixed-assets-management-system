0d021467ad1b597c6426514d482f5d7bc8b7b735
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-08 19:07:32 +0100

    * Updated navigation added custom font family

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,16 +1,21 @@
-<div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-        <a href="{{ route('home') }}" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span class="fs-5 d-none d-sm-inline">{{ \App\Configs\AppConfig::SHORT_APP_NAME }}</span>
-        </a>
-        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-            @foreach(\App\Configs\MainNavigationConfig::getMainNavigation() as $routeName => $routeMeta)
-                <li class="nav-item">
-                    <a href="{{ $routeMeta['route'] }}" class="nav-link align-middle px-0">
-                        <i class="fs-4 {{ $routeMeta['icon'] }}"></i> <span class="ms-1 d-none d-sm-inline">{{ $routeName }}</span>
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-    </div>
+<div class="container-flow">
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('home') }}">{{ \App\Configs\AppConfig::SHORT_APP_NAME }}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse container" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    @foreach(\App\Configs\MainNavigationConfig::getMainNavigation() as $routeName => $routeMeta)
+                        <li class="nav-item">
+                            <a href="{{ $routeMeta['route'] }}" class="nav-link">
+                                <i class="fs-4 {{ $routeMeta['icon'] }}"></i> <span>{{ $routeName }}</span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </nav>
 </div>
