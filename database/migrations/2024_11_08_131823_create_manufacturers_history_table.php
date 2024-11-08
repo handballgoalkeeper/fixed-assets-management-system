388e6ae8e7db4943b03d4eb40e222db29f377caf
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-08 14:34:02 +0100

    * Created migration for manufacturer audit table

---
+++ database/migrations/2024_11_08_131823_create_manufacturers_history_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create(table: \App\Models\ManufacturerHistoryModel::TABLE, callback: function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('manufacturer_id');
+            $table->string('action', 6);
+            $table->string('name', 255);
+            $table->string('description', 255)->nullable();
+            $table->boolean('is_active');
+            $table->unsignedBigInteger('modified_by');
+            $table->timestamp('timestamp')->useCurrent();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists(table: \App\Models\ManufacturerHistoryModel::TABLE);
+    }
+};
