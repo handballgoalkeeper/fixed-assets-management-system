d1fd1477d18a8d439b474318bd195e83ccf0542b
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-09 16:02:25 +0100

    * Added new message for user not authenticated

--- app/Enums/ErrorMessage.php
+++ app/Enums/ErrorMessage.php
@@ -5,4 +5,5 @@ namespace App\Enums;
 enum ErrorMessage: string
 {
     case UNHANDLED_EXCEPTION = "Unhandled exception occurred, please contact support.";
+    case USER_NOT_AUTHENTICATED = "User not authenticated, please contact support.";
 }
