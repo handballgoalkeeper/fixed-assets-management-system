d0c6dd47ed19145c684aa0f4c2c68d72bf4240bf
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-11 21:48:27 +0100

    * Created seeder and factory for departments

---
+++ database/factories/DepartmentModelFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\DepartmentModel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
+class DepartmentModelFactory extends Factory
+{
+    protected $model = DepartmentModel::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->generateUniqueDepartmentName(),
+            'description' => $this->faker->sentence(),
+            'last_modified_by' => DB::table('users')->inRandomOrder()->first()->id,
+        ];
+    }
+
+    private function generateUniqueDepartmentName(): string
+    {
+        $faker = $this->faker;
+        $departmentName = $faker->word();
+
+        while (DB::table(DepartmentModel::TABLE)->where('name', '=', $departmentName)->exists()) {
+            $departmentName = $faker->word();
+        }
+
+        return $departmentName . 'Department';
+    }
+}
