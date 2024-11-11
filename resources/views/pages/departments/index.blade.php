e986c3ebb1c11a82950e745121cfdead4d56a724
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-11 22:36:07 +0100

    * Added error and success handling, table for manufacturersr and pagination

--- resources/views/pages/departments/index.blade.php
+++ resources/views/pages/departments/index.blade.php
@@ -5,5 +5,45 @@
 @endsection
 
 @section('content')
-
+    @include('partials.successAlert')
+    @include('partials.errorAlert')
+    @if(!is_null($departments))
+        @include('partials.pagination', ['paginator' => $departments])
+        <div class="container-flow table-responsive">
+            <table class="table table-striped table-responsive-sm">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($departments as $department)
+                    <tr>
+                        <th scope="row">{{ $loop->iteration }}</th>
+                        <td>{{ $department->name }}</td>
+                        <td>{{ $department->description }}</td>
+                        @if( $department->is_active === 1 )
+                            <td class="text-success">Active</td>
+                        @else
+                            <td class="text-danger">Inactive</td>
+                        @endif
+                        <td>
+                            <a href="{{ route(name: 'manufacturers.permalink', parameters: [ 'manufacturer' => $department->id] ) }}" class="btn btn-outline-primary">
+                                View
+                            </a>
+                            <a href="{{ route(name: 'manufacturers.history', parameters: [ 'manufacturer' => $department->id] ) }}" class="btn btn-outline-secondary">
+                                History
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        @include('partials.pagination', ['paginator' => $departments])
+    @endif
 @endsection
