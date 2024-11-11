8fc2128ed358b3aee766fd08c6f332bd632a70e7
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-11 21:48:50 +0100

    * Added use factory

--- app/Models/DepartmentModel.php
+++ app/Models/DepartmentModel.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DepartmentModel extends Model
 {
+    use HasFactory;
+
     const TABLE = "departments";
 
     protected $table = self::TABLE;
