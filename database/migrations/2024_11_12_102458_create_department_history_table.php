7f117f44f199cb5fff969d48fb9fb52e1cb4d6ca
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-12 11:35:28 +0100

    * Created model and migration for department history

---
+++ database/migrations/2024_11_12_102458_create_department_history_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\DepartmentHistoryModel;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create(table: DepartmentHistoryModel::TABLE, callback: function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger(column: 'department_id')->nullable();
+            $table->string(column: 'action', length: 6);
+            $table->string(column: 'name', length: 255)->nullable();
+            $table->string(column: 'description', length: 255)->nullable();
+            $table->boolean(column: 'is_active');
+            $table->unsignedBigInteger(column: 'modified_by')->nullable();
+            $table->timestamp(column: 'timestamp')->useCurrent();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists(table: DepartmentHistoryModel::TABLE);
+    }
+};
