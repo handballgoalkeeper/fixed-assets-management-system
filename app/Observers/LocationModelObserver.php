99e80757fda0fe97a15e440af1cae9240513daf0
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-11-14 21:48:58 +0100

    * Created location model observer and registered it in location model

---
+++ app/Observers/LocationModelObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\LocationHistoryModel;
+
+class LocationModelObserver
+{
+    public function created(LocationHistoryModel $locationHistoryModel): void
+    {
+        //
+    }
+    
+    public function updated(LocationHistoryModel $locationHistoryModel): void
+    {
+        //
+    }
+}
