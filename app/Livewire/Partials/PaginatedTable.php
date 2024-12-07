320758574ec0cd971d59fc5663eba1dad18cafff
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2024-12-07 17:15:10 +0100

    * Implemented paginated table partial

---
+++ app/Livewire/Partials/PaginatedTable.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Livewire\Partials;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\View\View;
+use Livewire\Component;
+use Livewire\Features\SupportPagination\WithoutUrlPagination;
+use Livewire\WithPagination;
+
+class PaginatedTable extends Component
+{
+    use WithPagination, WithoutUrlPagination;
+
+// Possibly implement custom persistance across re-renders, so it can be private...
+    public string $table;
+
+    private LengthAwarePaginator $paginator;
+    public array $columnMapping;
+    public bool $hasIndexColumn;
+    public bool $hasViewBtn;
+    public bool $hasStatusColumn;
+    public bool $hasActionsEnabled;
+    public bool $hasHistoryBtn;
+    public int $perPage;
+
+    /**
+     * @param string $tableName
+     * @param array<string, string> $columnMapping
+     * @param bool $hasIndexColumn
+     * @param bool $hasViewBtn
+     * @param bool $hasStatusColumn
+     * @param bool $hasHistoryBtn
+     * @param int $perPage
+     */
+    public function mount(
+        string $tableName,
+        array $columnMapping,
+        bool $hasIndexColumn = false,
+        bool $hasViewBtn = false,
+        bool $hasStatusColumn = false,
+        bool $hasHistoryBtn = false,
+        int $perPage = 10
+    ): void
+    {
+        if (!Schema::hasTable($tableName)) {
+            $this->addError('table', "Table '$tableName' you specified does not exist.");
+        }
+
+        $this->table = $tableName;
+        $this->columnMapping = $columnMapping;
+        $this->hasIndexColumn = $hasIndexColumn;
+        $this->hasViewBtn = $hasViewBtn;
+        $this->hasStatusColumn = $hasStatusColumn;
+        $this->hasHistoryBtn = $hasHistoryBtn;
+        $this->perPage = $perPage;
+
+        $this->hasActionsEnabled = in_array(needle: true, haystack: [
+            $this->hasViewBtn,
+            $this->hasHistoryBtn
+        ],strict: true);
+
+    }
+
+    public function render(): View
+    {
+        $paginator = $this->getPaginator(perPage: $this->perPage);
+
+        return view('livewire.partials.paginated-table', data: [
+            'paginator' => $paginator
+        ]);
+    }
+
+    private function getPaginator(int $perPage): LengthAwarePaginator
+    {
+        $builder = DB::table(table: $this->table);
+
+        $columnsToSelect = [];
+
+        foreach ($this->columnMapping as $tableColumnName => $dbColumnName) {
+            $columnsToSelect[] = $dbColumnName . ' AS ' . $tableColumnName;
+        }
+
+        if ($this->hasStatusColumn) {
+            $columnsToSelect[] = 'is_active AS Status';
+        }
+
+        $builder->select(columns: $columnsToSelect);
+
+        return $builder->paginate(perPage: $perPage);
+    }
+}
---
+++ resources/views/livewire/partials/paginated-table.blade.php
@@ -0,0 +1,57 @@
+<section>
+        <div class="d-flex justify-content-center mt-4">
+            {{ $paginator->links() }}
+        </div>
+        <div class="container-flow table-responsive">
+            <table class="table table-striped table-responsive-sm">
+                <thead>
+                <tr>
+                    @if($hasIndexColumn)
+                        <th scope="col">#</th>
+                    @endif
+                    @foreach($columnMapping as $columnName => $_)
+                        <th scope="col">{{ $columnName }}</th>
+                    @endforeach
+                    @if($hasStatusColumn)
+                        <th scope="col">Status</th>
+                    @endif
+                    @if($hasActionsEnabled)
+                        <th scope="col">Actions</th>
+                    @endif
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach($paginator as $row)
+                        <tr>
+                            @if($hasIndexColumn)
+                                <th scope="row">{{ $loop->iteration }}</th>
+                            @endif
+                            @foreach($columnMapping as $columnName => $_)
+                                <td>{{ $row->{$columnName} }}</td>
+                            @endforeach
+                            @if($hasStatusColumn)
+                                @if( $row->Status === 1 )
+                                    <td class="text-success">Active</td>
+                                @else
+                                    <td class="text-danger">Inactive</td>
+                                @endif
+                            @endif
+                            @if($hasActionsEnabled)
+                                <td>
+                                    @if($hasViewBtn)
+                                        <a href="#" class="btn btn-outline-primary">View</a>
+                                    @endif
+                                    @if($hasHistoryBtn)
+                                        <a href="#" class="btn btn-outline-secondary">History</a>
+                                    @endif
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-content-center mt-4">
+            {{ $paginator->links() }}
+        </div>
+</section>
